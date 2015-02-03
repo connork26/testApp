ed875063b320eda2cc0d8edf8dd2034b1d9dd124
student <none@none.com>
2015-02-02 17:10:10 -0800

    Initial Commit

---
+++ firstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstProject
+//
+//  Created by student on 2/2/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstProject
+//
+//  Created by student on 2/2/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
