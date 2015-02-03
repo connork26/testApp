80d4769e2c3392d92ef4f03e02a41363be2b1dde
student <none@none.com>
2015-02-02 18:19:33 -0800

    First commit

--- firstProject/ViewController.m
+++ firstProject/ViewController.m
@@ -15,8 +15,33 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Loaded");
+    NSURL * url = [NSURL URLWithString:@"http://static01.nyt.com/images/2015/02/08/education/08football-1/08football-1-largeHorizontal375.jpg"];
+    NSData * data = [NSData dataWithContentsOfURL:url];
+    UIImage * image = [UIImage imageWithData:data];
+    
+    UIImageView * footballPic = [[UIImageView alloc] initWithImage:image];
+    [self.view  addSubview:footballPic];
+    
+    CGRect frame = [[UIScreen mainScreen] applicationFrame];
+    CGRect imageFrame = CGRectMake(100, 200, 150, 150);
+    
+    footballPic.frame = imageFrame;
+    
+    CGRect buttonFrame = CGRectMake((frame.size.width - 100) / 2, 400, 100, 35);
+    UIButton * button = [[UIButton alloc] initWithFrame:buttonFrame];
+    [self.view addSubview:button];
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    [button setBackgroundColor:[UIColor redColor]];
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void) buttonPressed:(UIButton *) sender {
+    NSLog(@"Button pressed");
+    [sender setBackgroundColor:[UIColor blueColor]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
